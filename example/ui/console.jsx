395cc7de81b181163dc2f177ce2e00c86aa93a34
brian <brian.shann@gmail.com>
2017-06-10 21:24:45 +0100

    better looking console element

--- example/ui/console.jsx
+++ example/ui/console.jsx
@@ -3,10 +3,29 @@ import React, { Component } from 'react';
 const consoleFn = window.console.log;
 const messages = [];
 
+function padNum(num){
+return ("0" + num).slice(-2);
+}
+
+function lineElem({time, message},index){
+  return <div key={time+message+index} style={{ backgroundColor: "rgba(255, 255, 255, 0.2)", marginBottom: 2, padding:"1px 2px"}}>
+            <span style={{ color:"gray"}}> {time} </span> <span style={{ color:"white"}}> { message } </span>
+        </div>
+}
+
 class ConsoleElem extends Component {
 
   componentDidMount(){
-    window.console.log = function(str){  messages.push(str)  }
+    window.console.log = function(){
+    consoleFn.apply(null,arguments)
+        const date = new Date();
+        const message = Array.prototype.slice.call(arguments).join(' ')
+        messages.unshift({message, time:padNum(date.getHours())
+        +":"+
+        padNum(date.getMinutes())
+        +"."+
+        padNum(date.getSeconds())})
+    }
   }
 
   componentWillUnmount(){
@@ -14,9 +33,31 @@ class ConsoleElem extends Component {
   }
 
   render() {
-    return <div style={{overflow:"scroll", padding:10, width:"100%", height:400, backgroundColor:"black" }}> {  messages.map( message => <div style={{ color:"white",   backgroundColor: "rgba(255, 255, 255, 0.2)",
-    marginBottom: 2}}> { message } </div> )  } </div>
+  
+  const consoleStyle = {
+      overflow:"hidden",
+      padding:10,
+      width:"90vw",
+      maxHeight:"25vh",
+      backgroundColor:"black",
+      position: "absolute",
+      bottom: 10, 
+      marginLeft: "auto",
+      marginRight: "auto",
+      left: 0,
+      right: 0,
+      borderRadius: 5
+  }
+  
+    return <div style={consoleStyle}>
+            {
+                messages.map( lineElem )
+            }
+           </div>
   }
 }
 
 export default ConsoleElem;
+
+
+
