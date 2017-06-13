d9b9bee9788ce9b904e89d7599753307e4c856f5
Brian <brian.shannon@davra.io>
2017-06-13 17:54:41 +0100

    added jest

---
+++ example/ui/__test__/require_context.js
@@ -0,0 +1,33 @@
+
+  const fs = require('fs');
+  const path = require('path');
+
+export default function (base = '.', scanSubDirectories = false, regularExpression = /\.js$/) {
+    const files = {};
+
+    function readDirectory(directory) {
+      fs.readdirSync(directory).forEach((file) => {
+        const fullPath = path.resolve(directory, file);
+
+        if (fs.statSync(fullPath).isDirectory()) {
+          if (scanSubDirectories) readDirectory(fullPath);
+
+          return;
+        }
+
+        if (!regularExpression.test(fullPath)) return;
+
+        files[fullPath] = true;
+      });
+    }
+
+    readDirectory(path.resolve(__dirname, base));
+
+    function Module(file) {
+      return require(file);
+    }
+
+    Module.keys = () => Object.keys(files);
+
+    return Module;
+  };
