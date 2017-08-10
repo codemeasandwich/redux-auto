81a4812861a9a2b3049f70f3c9a7ed0aad9f05b9
Brian <brian.shannon@davra.io>
2017-08-10 08:49:45 +0100

    removed unfinished tests from example

--- example/store/user/index.test.js
+++
@@ -1,9 +0,0 @@
-
-describe('test describe', () => {
-
-//+ should throw when using mergeReducers before "auto"
-//++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-    it('should test', () => { expect(true).toBe(true) })
-
-})
